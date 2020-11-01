140a4b27b18c7639ebbf3f819f51b398269ef31f
ppigazzini <pasquale.pigazzini@gmail.com>
2020-11-01 12:52:03 +0100

    Log the script 02_install_worker.sh

--- 02_install_worker.cmd
+++ 02_install_worker.cmd
@@ -1,3 +1,3 @@
 @echo off
 ::install fishtest worker
-C:\tools\msys64\msys2_shell.cmd -defterm -mingw64 -here -c "bash 02_install_worker.sh"
+C:\tools\msys64\msys2_shell.cmd -defterm -mingw64 -here -c "bash 02_install_worker.sh 2>&1 | tee 02_install_worker.sh.log"
